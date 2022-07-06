6b655a17a8b8e543b8033beb0bed804b5fdca671
Koroleva Svetlana <koroleva.4ne@gmail.com>
2022-07-06 15:47:44 +0500

    Add promo with media

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    close = document.querySelector('.menu__close');
+
+    hamburger.addEventListener('click', () => {
+        menu.classList.add('active');
+    });
+
+    close.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+})
